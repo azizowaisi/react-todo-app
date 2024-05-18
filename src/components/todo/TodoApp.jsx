0cc43e347b9a4b91bf75323237e30d2435ec02c9
“Abdul <azizowaisi@gmail.com>
2024-05-18 20:23:25 +0200

    making Todoapp component

---
+++ src/components/todo/TodoApp.jsx
@@ -0,0 +1,16 @@
+export default function TodoApp(){
+    return (
+        <div className="TodoApp">
+            Todo Application Management
+            <LoginComponent></LoginComponent>
+        </div>
+    )
+}
+
+function LoginComponent(){
+    return(
+        <div className="Login">
+            Login Component
+        </div>
+    )
+}
