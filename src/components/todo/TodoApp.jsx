9d9f4525d83a8de654d15b89ab29e838e8149245
“Abdul <azizowaisi@gmail.com>
2024-05-19 18:32:24 +0200

    varible as paramerters

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom"
+import { BrowserRouter, Route, Routes, useNavigate, useParams } from "react-router-dom"
 import "./TodoApp.css"
 
 export default function TodoApp(){
@@ -9,7 +9,7 @@ export default function TodoApp(){
             <Routes>
                 <Route path='/' element={<LoginComponent />}></Route>
                 <Route path='/login' element={<LoginComponent />}></Route>
-                <Route path='/welcome' element={<WelcomeComponent />}></Route>
+                <Route path='/welcome/:username' element={<WelcomeComponent />}></Route>
                 <Route path='/*' element={<ErrorComponent />}></Route>
             </Routes>
             </BrowserRouter>
@@ -37,7 +37,7 @@ function LoginComponent(){
         if(username === "azizowaisi" && password === "dummy"){
             setShowErrorMessage(false)
             setShowSuccessMessage(true)
-            navigate('/welcome')
+            navigate(`/welcome/${username}`)
             console.log('success')
 
         } else {
@@ -71,9 +71,13 @@ function LoginComponent(){
 }
 
 function WelcomeComponent(){
+
+    let {username} = useParams()
+    console.log(username)
+
     return(
         <div className="Welcome">
-            <h1>Welcome</h1>
+            <h1>Welcome {username}</h1>
             <div >
             Welcome Component
             </div>
